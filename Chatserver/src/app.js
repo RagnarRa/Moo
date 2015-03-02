49146ce4c217e5c4ec25b85c4faec0ad4cd88059
Ragnar Borgþór Ragnarsson <ragnarbr1992@gmail.com>
2015-03-02 16:23:07 +0000

    Set up package.json, package.bower, installed angular and karma.

--- Chatserver/src/app.js
+++
@@ -1,5 +0,0 @@
-angular.module("ChatApp", []);
-
-angular.module("ChatApp").controller("MainController", function($scope) { 
-   $scope.message = "Hello world";
-});
--- README.md
+++
@@ -1,1 +0,0 @@
-# Vefforritun-2
