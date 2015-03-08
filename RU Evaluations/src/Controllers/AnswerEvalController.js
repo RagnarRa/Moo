d727b2c766138885eed995a426d7cbf2a5996e1f
Ragnar Borgþór Ragnarsson <ragnarbr1992@gmail.com>
2015-03-08 18:12:46 +0000

    Added a better test assessment.

--- RU Evaluations/src/Controllers/AnswerEvalController.js
+++ RU Evaluations/src/Controllers/AnswerEvalController.js
@@ -1,8 +1,11 @@
 angular.module("evaluationApp").controller("AnswerEvalController", ["$scope", "$location", "$routeParams", "TemplateService", function($scope, $location, $routeParams, TemplateService) {	
+	$scope.evaluation = {};
+	$scope.evaluationAnswers = []; 
 	var courseID = $routeParams.courseID, semester = $routeParams.semester, evalID = $routeParams.evalID;
 	console.log("Course ID " + courseID + ", semester: " + semester + ", evalID: " + evalID); 
 
 	TemplateService.getEvaluationForCourseByEvalID(courseID, semester, evalID).success(function(data) {
 		console.dir(data);
+		$scope.evaluation = data; 
 	});
 }]);
