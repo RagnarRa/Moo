080e2bb293d057e0e11dfaf878c9766829adb725
guttih <gudjonholm@gmail.com>
2015-03-17 12:53:35 +0000

    test að koma fyrir adminController
    ekki alveg búið samnt

--- RU Evaluations/tests/AdminControllerSpec.js
+++ RU Evaluations/tests/AdminControllerSpec.js
@@ -1,7 +1,7 @@
 describe('AdminController', function() {
     beforeEach(module('evaluationApp'));
 
-    var authRequestHandler, ctrl, scope, location, filter, UserService, TemplateService;
+    var authRequestHandler, ctrl, scope, location, filter, UserService, templateService, $httpBackend, authRequestHandler;
 
     beforeEach(inject(function ($controller, $rootScope, $location, $filter, _UserService_, _TemplateService_) {
 
@@ -9,44 +9,59 @@ describe('AdminController', function() {
         location        = $location;
         filter          = $filter;
         UserService     = _UserService_;
-        TemplateService = _TemplateService_;
-
+        templateService = _TemplateService_;
 
         // Create the controller
         ctrl = $controller('AdminController', {
-            $scope: scope
-           /* $location: location*/
+            $scope: scope,
+            TemplateService: templateService
         });
     }));
 
-    /*beforeEach(inject(function($injector) {
+    beforeEach(inject(function($injector) {
         // Set up the mock http service responses
         $httpBackend = $injector.get('$httpBackend');
         // backend definition common for all tests
-        authRequestHandler = $httpBackend.when('POST', 'http://dispatch.ru.is/demo/api/v1/admin')
+        authRequestHandler = $httpBackend.when('GET', 'http://dispatch.ru.is/demo/api/v1/evaluationtemplates')
             .respond(null, null); //.respond(data, headers);
     }));
-
 
     afterEach(function() {
         $httpBackend.verifyNoOutstandingExpectation();
         $httpBackend.verifyNoOutstandingRequest();
-    });*/
+    });
 
     it("should call /createeval ", function() {
         spyOn(location, 'path');
+
         scope.createEval();
+        $httpBackend.flush();
         expect(location.path).toHaveBeenCalledWith('/createeval');
     });
 
     it("should opens an evaluation for students ", function() {
-
+        authRequestHandler = $httpBackend.when('POST', 'http://dispatch.ru.is/demo/api/v1/evaluations')
+            .respond(null, null); //.respond(data, headers);
         scope.startDate = new Date();
         scope.endDate = new Date();
         scope.endDate.setDate(scope.endDate.getDate() + 1);
         scope.templateID = 1 ;
         scope.addTemplate();
+        $httpBackend.flush();
         expect(scope.successMsg).toBe('Evaluations successfully added');
+    });
+
+    it("should fail to open an evaluation for students ", function() {
+        authRequestHandler = $httpBackend.when('POST', 'http://dispatch.ru.is/demo/api/v1/evaluations')
+            .respond(null, null); //.respond(data, headers);
+        authRequestHandler.respond(401, 'status', 'headers', 'config');
+        scope.startDate = new Date();
+        scope.endDate = new Date();
+        scope.endDate.setDate(scope.endDate.getDate() + 1);
+        scope.templateID = 1 ;
+        scope.addTemplate();
+        $httpBackend.flush();
+        expect(scope.successMsg).toBe('Error, unable to add evaluation');
     });
 
 
